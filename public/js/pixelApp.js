83487579fef24399a2516eb3ea1a80f97fc439f2
owlsketch <satimidus@gmail.com>
2018-05-17 17:48:44 -0400

    Testing HTTP requests on device

--- public/js/pixelApp.js
+++ public/js/pixelApp.js
@@ -6,11 +6,13 @@ import CanvasState from './pixelApp/CanvasStateConstructor.js';
 import ViewState from './pixelApp/ViewStateConstructor.js';
 import ColorPalette from './pixelApp/ColorPaletteConstructor.js';
 
-let jwt = 'JWTokenHere';
+let base_url = 'https://rugrid.mozilla-iot.org/things/http---192.168.33.132-3000/';
+//let base_url = 'http://desktop-1cjceb2.local:3000/properties/on';
+let jwt = 'YOUR.WEBTOKEN.HERE';
 
 let pxl = {};
 
-pxl.comm = new SocketState();
+pxl.comm = null; //new SocketState();
 pxl.ws = new WebSocketState(jwt);
 pxl.touches = new TouchHandler();
 pxl.mouse = new MouseHandler("mouse", 0, 0);
@@ -18,6 +20,17 @@ pxl.grid = new CanvasState(document.getElementById('editor'), pxl.comm, pxl.touc
 pxl.view = new ViewState(document.getElementById('view'), pxl.grid, pxl.touches);
 pxl.palette = new ColorPalette(document.getElementById('colorPalette'), pxl.grid, document.getElementById('appTitle'));
 
+let url = 'https://rugrid.mozilla-iot.org/things/http---192.168.33.132-3000/properties/on?jwt=' + jwt;
+let auth = 'Bearer ' + jwt;
+let headers = {
+  Authorization: auth,
+  Accept: 'application/json'
+};
+
+fetch(url, { headers: headers })
+  .then(function(res) {
+    console.log(res);
+  });
 
 pxl.comm.socket.on('boardConnect', function(data) {
   pxl.grid.pWidth = data.dimmensions.width;
